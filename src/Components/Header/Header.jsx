58d4901014be7723abe0f95150bf8edbaa0729f2
Caio Balduino <caiobalduinodev@hotmail.com>
2024-09-25 14:25:57 -0300

    ✨ feat: Create initial header

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,16 +1,20 @@
 import React from "react";
 import styles from "./Header.module.css";
 import { Link } from "react-router-dom";
+import Dog from "../../Assets/dogs.svg?react";
 
 const Header = () => {
   return (
-    <div className={styles.header}>
-      <nav>
-        Header
-        <Link to="/">Home</Link>
-        <Link to="/login">Login / Criar</Link>
+    <header className={styles.header}>
+      <nav className={`${styles.nav} container`}>
+        <Link className={styles.logo} to="/" aria-label="Dogs - Home">
+          <Dog />
+        </Link>
+        <Link className={styles.login} to="/login">
+          Login / Criar
+        </Link>
       </nav>
-    </div>
+    </header>
   );
 };
 
