53e7548d0120a73320f08088f05ddd3d6977659a
Caio Balduino <caiobalduinodev@hotmail.com>
2024-09-25 14:56:28 -0300

    ✨ feat: Create user login scope

---
+++ src/Components/Login/LoginForm.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const LoginForm = () => {
+  const [username, setUsername] = React.useState("");
+  const [password, setPassword] = React.useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    fetch("https://dogsapi.origamid.dev/json/jwt-auth/v1/token", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ username, password }),
+    }).then((res) => {
+      console.log(res);
+      return res.json()
+    }).then(json => {
+      console.log(json);
+      
+    });
+  }
+
+  return (
+    <section>
+      <h1>Login</h1>
+      <form action="" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={username}
+          onChange={({ target }) => setUsername(target.value)}
+        />
+        <input
+          type="password"
+          value={password}
+          onChange={({ target }) => setPassword(target.value)}
+        />
+        <button>Entrar</button>
+      </form>
+      <Link to="/login/criar">Cadastro</Link>
+    </section>
+  );
+};
+
+export default LoginForm;
