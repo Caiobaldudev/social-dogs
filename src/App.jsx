8baddc593a4a520976497ba567d6035ed36351b3
Caio Balduino <caiobalduinodev@hotmail.com>
2024-09-27 17:18:39 -0300

    ✨ feat: Create initial context for user login

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,20 @@ import Footer from "./Components/Footer/Footer";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./Components/Home/Home";
 import Login from "./Components/Login/Login";
+import { UserStorage } from "./UserContext";
 
 const App = () => {
   return (
     <div>
       <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login/*" element={<Login />} />
-        </Routes>
-        <Footer />
+        <UserStorage>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login/*" element={<Login />} />
+          </Routes>
+          <Footer />
+        </UserStorage>
       </BrowserRouter>
     </div>
   );
