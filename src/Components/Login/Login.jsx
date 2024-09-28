db1d134fae318d8dfcbd2dd650962ceb9745d7bb
Caio Balduino <caiobalduinodev@hotmail.com>
2024-09-28 01:54:36 -0300

    ✨ feat: Validate user token, handle login/logout, and implement navigation

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,11 +1,16 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import LoginForm from "./LoginForm";
 import LoginCreate from "./LoginCreate";
 import LoginPasswordLost from "./LoginPasswordLost";
 import LoginPasswordReset from "./LoginPasswordReset";
+import { UserContext } from "../../UserContext";
 
 const Login = () => {
+  const {login} = React.useContext(UserContext)
+
+  if (login === true) return <Navigate to="/conta"/>
+
   return (
     <div>
       <Routes>
