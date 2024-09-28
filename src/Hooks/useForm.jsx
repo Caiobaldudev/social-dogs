8b28f921c4ffe213ab305cf4be4d5d257dc9af9c
Caio Balduino <caiobalduinodev@hotmail.com>
2024-09-28 15:16:36 -0300

    ✨ feat: Add password validation regex

--- src/Hooks/useForm.jsx
+++ src/Hooks/useForm.jsx
@@ -6,6 +6,10 @@ const types = {
       /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
     message: "Preencha um email válido!",
   },
+  password: {
+    regex: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/,
+    message: 'A senha precisa conter 1 caracter maíusculo, 1 minúsculo e 1 dígito. Com no mínimo 8 caracteres' 
+  }
 };
 
 const useForm = (type) => {
