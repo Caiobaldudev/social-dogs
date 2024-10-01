2861bb2d46b98e15a228c319b99fad149df0b448
Caio Balduino <caiobalduinodev@hotmail.com>
2024-10-01 17:25:47 -0300

    ✨ feat: Add comments to posts

--- src/Components/Photo/PhotoComments.jsx
+++ src/Components/Photo/PhotoComments.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React, { useState } from "react";
+import { UserContext } from "../../UserContext";
+import PhotoCommentsForm from "./PhotoCommentsForm";
+import styles from 'PhotoComments.module.css'
 
-const PhotoComments = () => {
+const PhotoComments = (props) => {
+  const { login } = React.useContext(UserContext);
+  const [comments, setComments] = React.useState(() => props.comments);
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <>
+      <ul className={styles.comments}>
+        {comments.map((comment) => (
+          <li key={comment.comment_ID}>
+            <b>{comment.comment_author}: </b> 
+            <span>{comment.comment_content}</span>
+          </li>
+        ))}
+      </ul>
+      {login && <PhotoCommentsForm id={props.id} setComments={setComments}/>}
+    </>
+  );
+};
 
-export default PhotoComments
+export default PhotoComments;
--- src/api.jsx
+++ src/api.jsx
@@ -84,3 +84,18 @@ export function PHOTO_GET(id) {
     },
   };
 }
+
+export function COMMENT_POST(id, body) {
+  return {
+    url: `${API_URL}/api/comment/${id}`,
+    options: {
+      method: "POST",
+      cache: "no-store",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer" + window.localStorage.getItem("token"),
+      },
+      body: JSON.stringify(body),
+    },
+  };
+}
