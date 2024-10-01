cb22fd4a4a22293b1199fa4564aaf6e68e9e309e
Caio Balduino <caiobalduinodev@hotmail.com>
2024-10-01 10:34:52 -0300

    ✨ feat: Add number regex validation to useForm hook

--- src/Hooks/useForm.jsx
+++ src/Hooks/useForm.jsx
@@ -9,6 +9,10 @@ const types = {
   password: {
     regex: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/,
     message: 'A senha precisa conter 1 caracter maíusculo, 1 minúsculo e 1 dígito. Com no mínimo 8 caracteres' 
+  },
+  number: {
+    regex: /^\d+$/,
+    message: 'Utilize apenas números.'
   }
 };
 
